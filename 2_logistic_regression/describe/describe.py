e7e1ee7a554a3b235caa706d7f0a6ad270dc77ee
Shuai Wang <shuwang@f0r11s15.clusters.42paris.fr>
2025-09-02 19:57:33 +0200

    add cal

--- 2_logistic_regression/describe/describe.py
+++ 2_logistic_regression/describe/describe.py
@@ -3,6 +3,35 @@ import sys
 from load_csv import load
 
 
+def ft_describe(series: pd.Series, title: str) -> None:
+	"""Function to describe the data series."""
+
+	try:
+		sum = 0.0
+		count = 0.0
+		mean = 0.0
+		min = float('inf')
+		max = float('-inf')
+		for element in series: 
+			if element == element:
+				if (element < min):
+					min = element
+				if (element > max):
+					max = element
+				count += 1
+				sum += element
+
+		mean = sum / count
+		print(f"[{title}]")
+		# print(f"Sum {sum:6f}")
+		print(f"Count {count:6f}")
+		print(f"Mean {mean:6f}")
+		print(f"Min {min:6f}")
+		print(f"Max {max:6f}")
+		print()
+	except Exception as e:
+		print("Error:", e)
+
 def main():
 	"""Test of reading data and print it."""
 
@@ -19,7 +48,11 @@ def main():
  
 		df_num = df.select_dtypes(include="number")
 		# print(df_num.sum())
-		print(df_num)
+
+		for col in df_num:
+			ft_describe(df_num[col], col)
+
+		# print(df_num)
 	
 	except Exception as e:
 		print("Error:", e)
