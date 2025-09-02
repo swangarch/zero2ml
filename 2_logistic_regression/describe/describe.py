6eea0570811d2cfa332650d39ebc29f27b865f34
Shuai Wang <shuwang@f6r5s13.clusters.42paris.fr>
2025-09-02 18:47:49 +0200

    add csv manipulation for data

--- 2_logistic_regression/describe/describe.py
+++ 2_logistic_regression/describe/describe.py
@@ -1,17 +1,25 @@
 import pandas as pd
 import sys
+from load_csv import load
+
 
 def main():
 	"""Test of reading data and print it."""
 
 	try:
 		argv = sys.argv
+		assert len(argv) == 2, "Wrong argument number."
 
-		data = pd.read_csv(argv[1])
-		df = pd.DataFrame(data)
+		df = load(argv[1])
+		if df is None:
+			sys.exit(1)
 
 		print(df)
-		# print(df.describe())  ##remove this
+		print("\033[33m", df.describe(), "\033[0m")  ##remove this, forbidden!!!!!!!!!!!!!!!!
+ 
+		df_num = df.select_dtypes(include="number")
+		# print(df_num.sum())
+		print(df_num)
 	
 	except Exception as e:
 		print("Error:", e)
