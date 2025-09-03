c4f138a47f95f64fc20b7ff98055393b87ce0c78
Shuai Wang <shuwang@f6r9s9.clusters.42paris.fr>
2025-09-03 20:48:58 +0200

    add visualization

--- 2_logistic_regression/describe/describe.py
+++ 2_logistic_regression/describe/describe.py
@@ -3,34 +3,39 @@ import sys
 from load_csv import load
 
 
-def ft_describe(series: pd.Series, title: str) -> None:
+def ft_describe(series: pd.Series, title: str, all: dict) -> None:
 	"""Function to describe the data series."""
 
-	try:
-		sum = 0.0
-		count = 0.0
-		mean = 0.0
-		min = float('inf')
-		max = float('-inf')
-		for element in series: 
-			if element == element:
-				if (element < min):
-					min = element
-				if (element > max):
-					max = element
-				count += 1
-				sum += element
+	# try:
+	sum = 0.0
+	count = 0
+	mean = 0.0
+	sortedLi = sorted(series)
+	err = 0.0
+	cleanLi = []
+	for elem in sortedLi: 
+		if elem == elem:
+			sum += elem
+			cleanLi.append(elem)
 
-		mean = sum / count
-		print(f"[{title}]")
-		# print(f"Sum {sum:6f}")
-		print(f"Count {count:6f}")
-		print(f"Mean {mean:6f}")
-		print(f"Min {min:6f}")
-		print(f"Max {max:6f}")
-		print()
-	except Exception as e:
-		print("Error:", e)
+	count = len(cleanLi)
+	mean = sum / count
+
+	for num in cleanLi:
+		err += (num - mean) ** 2
+	
+	std = (err / count) ** 0.5
+	
+	min_num = cleanLi[0]
+	max_num = cleanLi[-1]
+
+	percent25 = cleanLi[int((count) / 4)]
+	percent50 = cleanLi[int((count) / 2)]
+	percent75 = cleanLi[int((count) / 4 * 3)]
+
+
+	all[title] = [count, mean, std, min_num, percent25, percent50, percent75, max_num]
+
 
 def main():
 	"""Test of reading data and print it."""
@@ -39,20 +44,27 @@ def main():
 		argv = sys.argv
 		assert len(argv) == 2, "Wrong argument number."
 
+		pd.set_option('display.float_format', '{:.6f}'.format)
 		df = load(argv[1])
 		if df is None:
 			sys.exit(1)
-
 		print(df)
+		print("--------------------pd describe----------------------")
 		print("\033[33m", df.describe(), "\033[0m")  ##remove this, forbidden!!!!!!!!!!!!!!!!
  
 		df_num = df.select_dtypes(include="number")
-		# print(df_num.sum())
-
+		
+		all = {"Feature": ["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "max"]}
+		
 		for col in df_num:
-			ft_describe(df_num[col], col)
+			ft_describe(df_num[col], col, all)
+		
+		print("--------------------my describe----------------------")
 
-		# print(df_num)
+		df_describ = pd.DataFrame(all)
+		df_describ = df_describ.set_index("Feature")
+		df_describ.index.name = None
+		print(df_describ)
 	
 	except Exception as e:
 		print("Error:", e)
---
+++ 2_logistic_regression/visualizer/pair_plot.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import sys
+from load_csv import load
+import matplotlib.pyplot as plt
+
+
+# def visualize_scatter(data: pd.DataFrame, feature1:str, feature2:str) -> None:
+# 	"""Visualize data."""
+	
+# 	try:
+# 		plt.scatter(data[feature1].values, data[feature2].values, s=1)
+# 		title = f"Scatter:{feature1} * {feature2}"
+# 		plt.title(title)
+# 		plt.xlabel(feature1)
+# 		plt.ylabel(feature2)
+# 		plt.show()
+# 	except Exception as e:
+# 		print("Error in scatter visualizer:", e)
+
+
+def visualize_scatter(data: pd.DataFrame, feature1:str, feature2:str, length:int, pos: int) -> None:
+	"""Visualize pair plot data."""
+	
+	try:
+		plt.subplot(length, length, pos)
+		plt.scatter(data[feature1].values, data[feature2].values, s=1)
+		# title = f"Scatter:{feature1} * {feature2}"
+		# plt.title(title)
+		# plt.xlabel(feature1)
+		# plt.ylabel(feature2)
+	except Exception as e:
+		print("Error in scatter visualizer:", e)
+
+
+def pair_plot_scatter(data: pd.DataFrame) -> None:
+	"""Show all the pair plots."""
+
+	features = data.iloc[0].index
+	print(data.iloc[0]) # debug
+
+	num_features = len(features)
+	count = 0
+	for f1 in features:
+		for f2 in features:
+			count += 1
+			if f1 != f2:
+				# print(f1, f2)
+				visualize_scatter(data, f1, f2, num_features, count)
+	plt.show()
+
+
+def main():
+	"""Program to visualize data."""
+
+	try:
+		argv = sys.argv
+		assert len(argv) == 2, "Wrong argument number."
+		df = load(argv[1])
+		if df is None:
+			sys.exit(1)
+
+		df_num = df.select_dtypes(include="number")
+		pair_plot_scatter(df_num)
+
+	except Exception as e:
+		print("Error:", e)
+
+
+if __name__ == "__main__":
+	main()
+
